ce2e7a4283622ec3e466c3aee01f3ef5310ba5e3
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-10-12 17:02:13 +0800

    Initial Commit

---
+++ 17Fit/_7FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7FitApp.swift
+//  17Fit
+//
+//  Created by 游哲維 on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct _7FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
